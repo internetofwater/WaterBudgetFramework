e3bb54679cee2fb9b6e2a4999493f1b8031dae47
Pierre Mishra <prashank.mishra2011@gmail.com>
2020-08-13 20:55:01 -0400

    round d3 on sample data

--- water_budget_webapp_pierre/www/exact_match_d3.js
+++ water_budget_webapp_pierre/www/exact_match_d3.js
@@ -229,9 +229,179 @@
 //     }
 
 
+/////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////
+// Above code doesn't work, below code works on flare.json but not on round_d3.json
+
+// var diameter = 960,
+//     radius = diameter / 2,
+//     innerRadius = radius - 120;
+
+// var cluster = d3.cluster()
+//     .size([360, innerRadius]);
+
+// // Lines linking nodes
+// var line = d3.lineRadial()   //changed radial line to line radial 
+//     .curve(d3.curveBundle.beta(0.85)) //amount of curve, change beta to see
+//     .radius(function(d) { return d.y; }) //when should the lines end, if you add a value to d.y the lines will become longer
+//     .angle(function(d) { return d.x / 180 * Math.PI; });
+
+// var svg = d3.select("body").append("svg")
+//     .attr("width", diameter)
+//     .attr("height", diameter)
+//   .append("g")
+//     .attr("transform", "translate(" + radius + "," + radius + ")");
+
+// var link = svg.append("g").selectAll(".link")
+// var node = svg.append("g").selectAll(".node");
+
+// d3.json("flare.json", function(error, data) {
+//   if (error) throw error;
+
+//   console.log(data)
+
+//   var root = packageHierarchy(data)
+//       .sum(function(d) { return d.size; });
+
+//   cluster(root);
+
+//   link = link
+//     .data(packageImports(root.leaves()))
+//     .enter().append("path")
+//       .each(function(d) { d.source = d[0], d.target = d[d.length - 1]; })
+//       .attr("class", "link")
+//       .attr("d", line);
+
+//   node = node
+//     .data(root.leaves())
+//     .enter().append("text")
+//       .attr("class", "node")
+//       .attr("dy", "0.31em")
+//       .attr("transform", function(d) { return "rotate(" + (d.x - 90) + ")translate(" + (d.y + 8) + ",0)" + (d.x < 180 ? "" : "rotate(180)"); })
+//       .attr("text-anchor", function(d) { return d.x < 180 ? "start" : "end"; })
+//       .text(function(d) { return d.data.key; })
+//       .on("mouseover", mouseovered)
+//       .on("mouseout", mouseouted)
+//       .style("font-weight", 300)
+//       .style("font-size", 11)
+//       .style("font-family", "arial")
+//       // .style("fill", "#bbb")
+//       // .on("mouseover", d => {d3.select(event.currentTarget)
+//       //   .style("fill", "#000")
+//       //   .style("font-weight", 700);})
+//       // .on("mouseout", d => {d3.select(event.currentTarget)
+//       //   .style("fill", "#bbb")
+//       //   .style("font-weight", 300);})
+//       ;
+      
+// });
+
+// function mouseovered(d) {
+//   node
+//       .each(function(n) { n.target = n.source = false; });
+
+//   link
+//       .classed("link--target", function(l) { if (l.target === d) return l.source.source = true; })
+//       .classed("link--source", function(l) { if (l.source === d) return l.target.target = true; })
+//     .filter(function(l) { return l.target === d || l.source === d; })
+//       .raise();
+
+//   node
+//       .classed("node--target", function(n) { return n.target; })
+//       .classed("node--source", function(n) { return n.source; });
+// }
+
+// function mouseouted(d) {
+//   link
+//       .classed("link--target", false)
+//       .classed("link--source", false);
+
+//   node
+//       .classed("node--target", false)
+//       .classed("node--source", false);
+// }
+
+// // Lazily construct the package hierarchy from class names (changed "classes" to "data").
+// function packageHierarchy(data) {
+//   var map = {};
+
+//   function find(name, data) {
+//     var node = map[name], i;
+//     if (!node) {
+//       node = map[name] = data || {name: name, children: []};
+//       if (name.length) {
+//         node.parent = find(name.substring(0, i = name.lastIndexOf(".")));
+//         node.parent.children.push(node);
+//         node.key = name.substring(i + 1);
+//       }
+//     }
+//     return node;
+//   }
+
+//   data.forEach(function(d) {
+//     find(d.name, d);
+//   });
+
+//   return d3.hierarchy(map[""]);
+// }
+
+// // Return a list of imports for the given array of nodes.
+// function packageImports(nodes) {
+//   var map = {},
+//       imports = [];
+
+//   // Compute a map from name to node.
+//   nodes.forEach(function(d) {
+//     map[d.data.name] = d;
+//   });
+
+//   // For each import, construct a link from the source to target node.
+//   nodes.forEach(function(d) {
+//     if (d.data.imports) d.data.imports.forEach(function(i) {
+//       imports.push(map[d.data.name].path(map[i]));
+//     });
+//   });
+
+//   return imports;
+// }
 
 
+/////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////
+// Editing above code to work for round_d3.json
 
+// const data = [{
+//   "name": "flare.analytics.DistritoFederal.NoEspecificado",
+//   "size": 3800,
+//   "imports": ["flare.analytics.DistritoFederal.GUSTAVOAMADERO", "flare.analytics.México.ATIZAPAN"]
+// }, {
+//   "name": "flare.analytics.DistritoFederal.AZCAPOTZALCO",
+//   "size": 3800,
+//   "imports": ["flare.analytics.DistritoFederal.GUSTAVOAMADERO", "flare.analytics.DistritoFederal.IZTAPALAPA", "flare.analytics.DistritoFederal.BENITOJUAREZ", "flare.analytics.DistritoFederal.CUAUHTEMOC", "flare.analytics.DistritoFederal.MIGUELHIDALGO", "flare.analytics.DistritoFederal.VENUSTIANOCARRANZA", "flare.analytics.México.TLALNEPANTLADEBAZ"]
+// }, {
+//   "name": "flare.analytics.DistritoFederal.COYOACAN",
+//   "size": 3800,
+//   "imports": ["flare.analytics.DistritoFederal.GUSTAVOAMADERO", "flare.analytics.DistritoFederal.IZTAPALAPA", "flare.analytics.DistritoFederal.LAMAGDALENACONTRERAS", "flare.analytics.DistritoFederal.ALVAROOBREGON", "flare.analytics.DistritoFederal.TLALPAN", "flare.analytics.DistritoFederal.BENITOJUAREZ", "flare.analytics.DistritoFederal.CUAUHTEMOC", "flare.analytics.DistritoFederal.MIGUELHIDALGO", "flare.analytics.México.TLALNEPANTLADEBAZ"]
+// }, {
+//   "name": "flare.analytics.DistritoFederal.CUAJIMALPADEMORELOS",
+//   "size": 3800,
+//   "imports": ["flare.analytics.DistritoFederal.GUSTAVOAMADERO", "flare.analytics.DistritoFederal.ALVAROOBREGON", "flare.analytics.DistritoFederal.TLALPAN", "flare.analytics.DistritoFederal.BENITOJUAREZ", "flare.analytics.DistritoFederal.CUAUHTEMOC", "flare.analytics.DistritoFederal.MIGUELHIDALGO"]
+// }
+// ]
+
+const data = [
+  {"name": "A", "imports": ["B", "C", "C", "D"]},
+  {"name": "B", "imports": ["A", "C", "D", "D"]},
+  {"name": "C", "imports": ["B", "D"]},
+  {"name": "D", "imports": ["B", "A"]},
+  {"name": "E", "imports": ["B", "A"]},
+  {"name": "F", "imports": ["B", "A"]},
+  {"name": "G", "imports": ["B", "A"]},
+  {"name": "H", "imports": ["A", "F", "I"]},
+  {"name": "I", "imports": ["G", "H"]}
+ ]
 
 var diameter = 960,
     radius = diameter / 2,
@@ -240,9 +410,10 @@ var diameter = 960,
 var cluster = d3.cluster()
     .size([360, innerRadius]);
 
-var line = d3.radialLine()
-    .curve(d3.curveBundle.beta(0.85))
-    .radius(function(d) { return d.y; })
+// Lines linking nodes
+var line = d3.lineRadial()   //changed radial line to line radial 
+    .curve(d3.curveBundle.beta(0.85)) //amount of curve, change beta to see
+    .radius(function(d) { return d.y; }) //when should the lines end, if you add a value to d.y the lines will become longer
     .angle(function(d) { return d.x / 180 * Math.PI; });
 
 var svg = d3.select("body").append("svg")
@@ -251,14 +422,18 @@ var svg = d3.select("body").append("svg")
   .append("g")
     .attr("transform", "translate(" + radius + "," + radius + ")");
 
-var link = svg.append("g").selectAll(".link"),
-    node = svg.append("g").selectAll(".node");
+var link = svg.append("g").selectAll(".link")
+var node = svg.append("g").selectAll(".node");
 
-d3.json("flare.json", function(error, classes) {
-  if (error) throw error;
+// d3.json("flare.json", function(error, data) {
+//   if (error) throw error;
 
-  var root = packageHierarchy(classes)
+  console.log(data)
+
+  var root = packageHierarchy(data)
       .sum(function(d) { return d.size; });
+
+  console.log(root)
 
   cluster(root);
 
@@ -267,7 +442,8 @@ d3.json("flare.json", function(error, classes) {
     .enter().append("path")
       .each(function(d) { d.source = d[0], d.target = d[d.length - 1]; })
       .attr("class", "link")
-      .attr("d", line);
+      .attr("d", line)
+    //  .style("stroke", "cyan");
 
   node = node
     .data(root.leaves())
@@ -278,8 +454,20 @@ d3.json("flare.json", function(error, classes) {
       .attr("text-anchor", function(d) { return d.x < 180 ? "start" : "end"; })
       .text(function(d) { return d.data.key; })
       .on("mouseover", mouseovered)
-      .on("mouseout", mouseouted);
-});
+      .on("mouseout", mouseouted)
+      .style("font-weight", 300)
+      .style("font-size", 11)
+      .style("font-family", "arial")
+      // .style("fill", "#bbb")
+      // .on("mouseover", d => {d3.select(event.currentTarget)
+      //   .style("fill", "#000")
+      //   .style("font-weight", 700);})
+      // .on("mouseout", d => {d3.select(event.currentTarget)
+      //   .style("fill", "#bbb")
+      //   .style("font-weight", 300);})
+      ;
+// });     
+
 
 function mouseovered(d) {
   node
@@ -306,8 +494,8 @@ function mouseouted(d) {
       .classed("node--source", false);
 }
 
-// Lazily construct the package hierarchy from class names.
-function packageHierarchy(classes) {
+// Lazily construct the package hierarchy from class names (changed "classes" to "data").
+function packageHierarchy(data) {
   var map = {};
 
   function find(name, data) {
@@ -323,7 +511,7 @@ function packageHierarchy(classes) {
     return node;
   }
 
-  classes.forEach(function(d) {
+  data.forEach(function(d) {
     find(d.name, d);
   });
 
@@ -349,3 +537,6 @@ function packageImports(nodes) {
 
   return imports;
 }
+
+
+
